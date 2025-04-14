f6dfa35563c2091642c5c08211b92b7b9fcb95cf
wassem-qaneer <s12325968@stu.najah.edu>
2025-04-14 18:01:40 +0300

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./component/nav-bar/nav-bar.component";
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent,RouterModule,FooterComponent],
+  imports: [RouterOutlet, HeaderComponent,RouterModule,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,18 @@ import { EmployerLoginComponent } from './component/employer-login/employer-logi
 import { EmployerRegisterComponent } from './component/employer-register/employer-register.component';
 import { Error404Component } from './component/error-404/error-404.component';
 import { ContactUsComponent } from './component/contact-us/contact-us.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 export const routes: Routes = [
-    {path : "" , component: Error404Component},
+    {path : "" , component: HomeComponent},
     { path: 'job-seeker-register', component: JobSeekerRegisterComponent },
     { path: 'job-seeker-login', component: JobSeekerLoginComponent },
     { path: 'employer-register', component: EmployerRegisterComponent },
     { path: 'employer-login', component: EmployerLoginComponent },
     { path: 'forgot-password', component: ForgotPasswordComponent },
     {path : 'contact-us' , component: ContactUsComponent},
+    {path : 'login' , component: LoginComponent},
+    {path : '**' , component: Error404Component},
 ]
